247053fb8182a2721fd69b2ab11caddc95885e01
Vik Ramanujam <vik.ramanujam@888holdings.com>
2020-03-11 11:51:38 +0000

    Use new useService syntax

--- src/services/cardService.js
+++ src/services/cardService.js
@@ -1,8 +1,19 @@
 import serviceBuilder from 'redux-peasy';
 import { API } from '~config';
 
-const getCards = serviceBuilder('card', async (state) => {
+const cardService = serviceBuilder('card', async (state) => {
     return (await fetch(API.CARDS)).json();
 });
 
-export default getCards;
+export const addNewCard = cardService.getNewAction(({cards}) => {
+    return {
+        cards: cards.data.push({
+            "name": "Eight of Cups",
+            "number": window.crypto.getRandomValues(new Uint32Array(1))[0],
+            "suit": "Cups",
+            "img": "c08.jpg"
+        }),
+    }
+})
+
+export default cardService;
--- src/views/CardsList/CardsList.jsx
+++ src/views/CardsList/CardsList.jsx
@@ -1,20 +1,22 @@
 import React from 'react';
 import { useService, useAppState } from 'redux-peasy';
-import cardService from 'services/cardService';
+import cardService, { addNewCard } from 'services/cardService';
 import { useTranslation } from 'react-i18next';
-import LoadingSVG from '../../loading.svg';
+import {ReactComponent as LoadingSVG} from '../../loading.svg';
 
 export default () => {
     const [t] = useTranslation();
     const [getCardsAction, clearCardsAction] = useService(cardService);
+    const [ addNewCardAction ] = useService(addNewCard);
     const { data: cardsList, isLoading } = useAppState('cards');
 
-    return isLoading
-        ? <LoadingSVG/>
-        : <div>
-            <h1>{t('CARD_HEADING')}</h1>
-            <button onClick={getCardsAction}>get cards</button>
-            <button onClick={clearCardsAction}>clear</button>
-            {cardsList && cardsList.map(({name, number}) => <div key={name+number}>{name}</div>)}
-        </div>
+    return <div>
+        <h1>{t('CARD_HEADING')}</h1>
+        <button onClick={getCardsAction}>get cards</button>
+        <button onClick={clearCardsAction}>clear</button>
+        <button disabled={!cardsList} onClick={addNewCardAction}>add</button>
+        {isLoading
+            ? <LoadingSVG height={30} width={30}/>
+            : (cardsList ||[]).map(({name}, index) => <div key={name+index}>{name}</div>)}
+    </div>
 }
